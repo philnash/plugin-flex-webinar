b3995dc485e516748cc58461dbec648d9796f28e
Phil Nash <philnash@gmail.com>
2021-05-25 10:35:53 +1000

    Removes dissmissable bar from plugin

--- src/states/index.js
+++ src/states/index.js
@@ -1,11 +1,8 @@
 import { combineReducers } from 'redux';
 
-import { reduce as CustomTaskListReducer } from './CustomTaskListState';
-
 // Register your redux store under a unique namespace
 export const namespace = 'flex-webinar';
 
 // Combine the reducers
 export default combineReducers({
-  customTaskList: CustomTaskListReducer
 });
